03b8bd92766ea92739338647bd05d51d5e379af6
Andrei Mikhailov <amkhlv@gmail.com>
2025-05-25 18:46:30 -0300

    improved Nix support

--- default.nix
+++ default.nix
@@ -1,2 +1,10 @@
-{ pkgs ? import <nixpkgs> { }, ... }:
-pkgs.haskellPackages.callCabal2nix "hdt" ./. { }
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+pkgs.haskell.lib.overrideCabal (pkgs.haskellPackages.callCabal2nix "hdt" ./. { }) (old: {
+  postInstall =
+    (old.postInstall or "")
+    + "\n"
+    + "mkdir -p $out/share/applications/; cp resources/hdt.desktop $out/share/applications/ ; \n";
+})
