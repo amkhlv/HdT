23a1b6bd68fdf6b7049758be3415b97ad90f1a0d
Andrei Mikhailov <amkhlv@gmail.com>
2025-06-05 21:56:32 -0300

    wait for external command to complete

--- src/Utils.hs
+++ src/Utils.hs
@@ -5,16 +5,20 @@
 
 module Utils (openEditor, openInkscape) where
 
+import Control.Monad (unless)
 import System.Directory (getHomeDirectory)
+import System.Exit (ExitCode (ExitSuccess))
 import qualified System.Process as SysProc
 
 openEditor :: String -> IO ()
 openEditor filename = do
   home <- getHomeDirectory
-  r <- SysProc.createProcess $ SysProc.proc (home ++ "/.config/hdt/edit-pdq.sh") [filename]
-  return ()
+  (_, _, _, handle) <- SysProc.createProcess $ SysProc.proc (home ++ "/.config/hdt/edit-pdq.sh") [filename]
+  code <- SysProc.waitForProcess handle
+  unless (code == ExitSuccess) $ print code
 
 openInkscape :: String -> IO ()
 openInkscape filename = do
-  r <- SysProc.createProcess $ SysProc.proc "inkscape" [filename]
-  return ()
+  (_, _, _, handle) <- SysProc.createProcess $ SysProc.proc "inkscape" [filename]
+  code <- SysProc.waitForProcess handle
+  unless (code == ExitSuccess) $ print code
