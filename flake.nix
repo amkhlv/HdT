72432011a2bdad43ff1300ffe437f429bab398fd
Andrei Mikhailov <amkhlv@gmail.com>
2025-07-20 15:16:09 -0300

    simplified flake

--- flake.nix
+++ flake.nix
@@ -14,50 +14,14 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        dflt = import ./default.nix { inherit pkgs; };
       in
       {
-        packages.default = import ./default.nix { inherit pkgs; };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cabal-install
-            haskell-language-server
-            cabal2nix
-            pkg-config
-            zlib
-            zlib.dev
-            bintools
-            glib
-            cairo
-            expat
-            pcre2
-            pcre.dev
-            hyperscan
-            xorg.libXdmcp
-            xorg.libXtst
-            pango
-            util-linux.dev
-            fribidi
-            libthai
-            libdatrie
-            gtk4
-            libxkbcommon
-            libepoxy
-            icu
-            gmp
-            zlib
-            haskellPackages.zlib
-            haskellPackages.gi-gtk_4
-            librsvg
-            poppler
-            poppler_gi
-            libsysprof-capture
-            libselinux
-            libsepol
-            lerc
-            dav1d
-            libxml2
-            wrapGAppsHook4
-          ];
+        packages.default = dflt;
+        devShells = {
+          default = pkgs.mkShell {
+            inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
+          };
         };
       }
     );
